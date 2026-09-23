64d6db1d4ef18448a23f889a450e4ce892f7f28e
Sawyer <W0876258@myscc.ca>
2026-09-22 23:21:14 -0700

    Begun to code within starting interface, and added array to app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import{ musicGlossary } from './models/Interface'
 
 @Component({
   imports: [RouterOutlet],
@@ -8,5 +9,17 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('untitled');
+
+    protected readonly title = signal('Music-Profile');
+
+    music: musicGlossary[] = [
+        {
+            id: 1,
+            name: "Test",
+            category: "metal",
+            year: 2020,
+            dateReleased: "2020-12-24"
+        }
+    ];
+
 }
